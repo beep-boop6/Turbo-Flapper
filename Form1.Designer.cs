78cc0d0a907adf406b1d528af685dfe56059ff0d
beep-boop6 <rorager@yandex.com>
2025-05-29 12:38:50 +0500

    first

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.flapper = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.startLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.flapper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
             this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.BackColor = System.Drawing.Color.NavajoWhite;
+            this.scoreText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scoreText.Font = new System.Drawing.Font("Ravie", 16F, System.Drawing.FontStyle.Bold);
+            this.scoreText.Location = new System.Drawing.Point(12, 9);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(170, 38);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // flapper
+            // 
+            this.flapper.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flapper.BackColor = System.Drawing.Color.Transparent;
+            this.flapper.Image = global::Turbo_Flapper.Properties.Resources.bird;
+            this.flapper.Location = new System.Drawing.Point(73, 571);
+            this.flapper.Name = "flapper";
+            this.flapper.Size = new System.Drawing.Size(104, 84);
+            this.flapper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flapper.TabIndex = 1;
+            this.flapper.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::Turbo_Flapper.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-10, 897);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(893, 125);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::Turbo_Flapper.Properties.Resources.pipe;
+            this.pipeBottom.Location = new System.Drawing.Point(636, 624);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(141, 680);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 2;
+            this.pipeBottom.TabStop = false;
+            this.pipeBottom.Visible = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::Turbo_Flapper.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(636, -351);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(141, 680);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 0;
+            this.pipeTop.TabStop = false;
+            this.pipeTop.Visible = false;
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startLabel.Location = new System.Drawing.Point(300, 400);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(319, 36);
+            this.startLabel.TabIndex = 5;
+            this.startLabel.Text = "Press ENTER to start";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1317, 673);
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(872, 1009);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.flapper);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.pipeTop);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Turbo Flapper";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.flapper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox flapper;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label startLabel;
     }
 }
 
---
+++ StartScreen.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Turbo_Flapper
+{
+    partial class StartScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.BackgroundImage = global::Turbo_Flapper.Properties.Resources.Group_1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(183, 658);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(336, 116);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.LoadGame);
+            // 
+            // StartScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(782, 953);
+            this.Controls.Add(this.button1);
+            this.Name = "StartScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StartScreen";
+            this.Load += new System.EventHandler(this.StartScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
